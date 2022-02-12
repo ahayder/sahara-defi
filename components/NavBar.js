dc49a87db4b3a337e0466851afc35c5e10167253
Ali Hayder <alihayder19@gmail.com>
2022-02-13 04:04:42 +0600

    Add Fullscreen Menu

--- components/NavBar.js
+++ components/NavBar.js
@@ -12,15 +12,17 @@ import {
   Stack,
   useColorMode,
   Image,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
   Heading,
   Text,
+  ButtonGroup,
+  Spacer,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon, HamburgerIcon } from "@chakra-ui/icons";
 
@@ -84,15 +86,83 @@ export default function NavBar() {
           </Stack>
         </Flex>
       </Flex>
-      {/* <Modal isOpen={isOpen} onClose={onClose} size={"full"}>
-        <ModalOverlay />
-          <ModalCloseButton />
-          <Flex>
-            <Heading>Sahara Token</Heading>
-            <Text>Hold, and earn BTC as reward.</Text>
-            <Text>Join us!</Text>
-          </Flex>
-      </Modal> */}
+      <Drawer
+        size={"full"}
+        placement={"right"}
+        onClose={onClose}
+        isOpen={isOpen}
+      >
+        <DrawerOverlay />
+        <DrawerContent bgColor={"gray.600"}>
+          <DrawerCloseButton
+            position={"absolute"}
+            top={5}
+            right={5}
+            width={"2rem"}
+            height={"2rem"}
+            color={"gray.100"}
+            bgColor={"gray.900"}
+          />
+          {/* <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader> */}
+          <DrawerBody>
+            <Image
+              position={"absolute"}
+              top={5}
+              left={5}
+              width={"3rem"}
+              src="/logo_yellow.png"
+              alt="Sahara Token Logo"
+            />
+            <Flex
+              color={"gray.100"}
+              direction={"column"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              height={"100vh"}
+            >
+              <Text fontSize={"6xl"}>Sahara Token</Text>
+              <Text fontSize={"2xl"}>Hold, and Earn BTC as Reward.</Text>
+              <Text fontSize={"2xl"} mb={10}>
+                Join us!
+              </Text>
+
+              <Button
+                mb={4}
+                color={"gray.900"}
+                variant={"solid"}
+                w={["90%", "80%", "50%", "30%"]}
+                p={6}
+                bg={"saharaYellow.900"}
+              >
+                $SATO Token
+              </Button>
+              <Button
+                mb={4}
+                color={"gray.900"}
+                variant={"solid"}
+                w={["90%", "80%", "50%", "30%"]}
+                p={6}
+                bg={"saharaYellow.900"}
+              >
+                Our Yellowpaper
+              </Button>
+              <Button
+                mb={4}
+                color={"gray.900"}
+                variant={"solid"}
+                w={["90%", "80%", "50%", "30%"]}
+                p={6}
+                bg={"saharaYellow.900"}
+              >
+                The Team
+              </Button>
+              <Button mb={4} color={"gray.100"} variant={"link"} w="30%">
+                Privacy Policy
+              </Button>
+            </Flex>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 }
